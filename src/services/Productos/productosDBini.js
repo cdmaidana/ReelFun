fd864b02557ff251388c34b477c9a6d33fac56ea
maidanacesar <maidanacesar@gmail.com>
2026-01-12 06:35:02 -0300

    prod para firebase

---
+++ src/services/Productos/productosDBini.js
@@ -0,0 +1,129 @@
+ 
+export const productos = [
+  {id:1,tipo:'reel',titulo:'Reel LINNHUE RR-AE2000',desc:'Reel Carrete de Pesca LINNHUE RR-AE2000 9 Niveles de Magnético',
+    precio:'33.999',img:'/img/prod/LINNHUE-RR-AE2000.webp',
+    especificaciones:{ 
+      "0":['tipo','rotativo'],
+      "1":['ladoManija','derecho'],
+      "2":['magnetico','9 niveles de magnético ajustable.'],
+      "3":['iman','imanes de neodimio N42'],
+      "4":['relTransmisión','6.3:1'],
+      "5":['salidaAgua','cilíndrica de cerámica, suave, fuerte y resistente al desgaste'],
+      "6":['fuerzaDescarga','8 kg'],
+      "7":['peso','197g'],
+      "8":['alarmaPique','si'],
+      "9":['capLinea','0,26 mm-140 m; 0,28 mm-120 m; 0,30 mm-110 m'],
+      "10":['materil','mango de metal CNC, empuñadura de eva, carrete de aleación de aluminio, cuerpo de nailon reforzado, engranaje de aluminio, varilla dentada de cobre.']
+	  }
+   } ,
+  {id:2,tipo:'reel',titulo:'Reel Frontal Amberjack 7000',desc:'Reel Frontal Amberjack 7000 12 Rul Pesca Variada 10kg Drag Color Negro Y Dorado 7000',
+    precio:'36.575',img:'/img/prod/Amberjack-7000.webp',
+    especificaciones:{ 
+      "0":['tipo','frontal'],
+      "1":['fuerzaDescarga','10 kg'],
+      "2":['rulemanes','12'],
+      "3":['relTransmisión','5.2:1'],
+      "4":['material','polimiero de alto impacto'],
+      "5":['perfil','Alto'],
+      "6":['capLinea','0.40mm-200mts'],
+      "7":['freno','Multidisco'],
+      "8":['alarmaPique','si']
+    }
+   },
+  {id:3,tipo:'reel',titulo:'Reel Huevito Caster Legend Pro 110',desc:'Reel Huevito Caster Legend Pro 110 Baitcasting 10 Rulemanes Color Gris - Manija Derecha',
+    precio:'108.289',img:'/img/prod/CasterLegendPro110.webp',
+    especificaciones:{ 
+      "0":['tipo','rotativo'],
+      "1":['fuerzaDescarga','8.16 kg'],
+      "2":['rulemanes','10'],
+      "3":['relTransmisión','7.2:1'],
+      "4":['material','Grafito'],
+      "5":['perfil','Bajo'],
+      "6":['capLinea','multifilamento 0.18/100 - monofilamento 0.30/120, multifilamento 0.25/100 - monofilamento 0.40/100'],
+      "7":['freno','Magnético'],
+      "8":['alarmaPique','si']
+    }
+   },
+  {id:4,tipo:'cana',titulo:'Caña Caster Sniper',desc:'Caña Caster Sniper 4.20m 3 Tramos Lance Costa Mar 100-200g',
+    precio:'89.475',img:'/img/prod/CasterSniper3Tramos.webp',
+    especificaciones:{
+      "0":['peso','70g'],
+      "1":['resistencia','100 a 200 gramos'],
+      "2":['acción','rápida'],
+      "3":['largo','4.2 m'],
+      "4":['material', 'fibra de vidrio'],
+      "5":['mango','corcho'],
+      "6":['guias','ceramica'],
+      "7":['tramos','3'] 
+     }
+   },
+  {id:5,tipo:'anzuelo',titulo:'Caja Anzuelos',desc:'Caja Con 300 Anzuelos Pesca Variada #9 Al #16',
+    precio:'24.500',img:'/img/prod/cajaAnzuelosVariada.webp',
+    especificaciones:{
+     "0": ['marca','Anmuka'], 
+      "1":['medida','3'],
+      "2":['cantidad','300'],
+      "3":['envase','caja'],
+      "4":['material','Acero inoxidable'] 
+    }
+   },
+  {id:6,tipo:'anzuelo',titulo:'Blister Anzuelos Pesca',desc:'Blister Anzuelos Pesca Variada #9 Al #16',
+    precio:'24.500',img:'/img/prod/blisterAnzuelosVariada.webp',
+    especificaciones:{
+      "0":['marca','Kamakura'], 
+      "1":['medida','2'],
+      "2":['cantidad','2'],
+      "3":['envase','blister'],
+      "4":['material', 'Acero al carbono' ]
+    }
+   },
+   {id:7,tipo:'plomada',titulo:'Plomada Municion Partida',desc:'Plomada Municion Partida Pesca Caja Organizadora 50 Unidades',
+    precio:'24.500',img:'/img/prod/plomadaMunicionPartida.webp',
+    especificaciones:{
+      "0":['marca','X-Fish'],
+      "1":['forma','Redonda'], 
+      "2":['medida','1g'],
+      "3":['cantidad','50'],
+      "4":['envase','blister']  
+    }
+   },
+  {id:8,tipo:'plomada',titulo:'Plomada Corrediza',desc:'Plomos Plomadas Corrediza 20grs 40grs Surtidas Rio Pack X10',
+    precio:'6.999',img:'/img/prod/plomadasCorrediza20.webp',
+    especificaciones:{
+      "0":['marca','Red Fish'],
+      "1":['forma','corrediza '], 
+      "2":['medida','40g'],
+      "3":['cantidad','10'],
+      "4":['envase','blister']
+    }
+   },
+  {id:9,tipo:'cana',titulo:'Caña Shimano Stimula 6.6',desc:'Caña Shimano Stimula 6.6 Pies 6-10lbs 2 Tramos Spinning',
+    precio:'60.705',img:'/img/prod/canashimanostimula66.webp',
+    especificaciones: {
+      "0":['peso','120g'],
+      "1":['resistencia','10lbs'],
+      "2":['acción','rápida'],
+      "3":['largo','1.98 m'],
+      "4":['material', 'grafito'],
+      "5":['mango','corcho'],
+      "6":['guias','ceramica'],
+      "7":['tramos','2'] 
+        }
+   } ,
+  {id:10,tipo:'cana',titulo:'Caña Okuma Morena Power 2.10',desc:'Caña Okuma Morena Power 2.10 1 Tr Trolling Rotativo 15-45 Lb',
+    precio:'234.710',img:'/img/prod/canamorenapowerokuma.webp',
+    especificaciones:{
+      "0":['peso','120g'],
+      "1":['resistencia','15-40lbs'],
+      "2":['acción','rápida'],
+      "3":['largo','2.1 m'],
+      "4":['material', 'grafito'],
+      "5":['mango','EVA'],
+      "6":['guias','oxido de aluminio'],
+      "7":['tramos','1'],
+      "8":['potencia','Pesada'], 
+      "9":['cantidad de guias','9'],
+    }
+   } 
+  ];   
+  
